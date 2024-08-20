739586ded20c737b336b9a1d496b92b87a4bd73b
LNUAKX <lukmaannadaf@gmail.com>
2024-08-20 14:30:58 +0530

    modify

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
-      <footer className="bg-black text-gray-800 text-xs m-2 max-sm:w-[100vw] p-2 flex justify-center items-center ">
+      <footer className="bg-black text-gray-700 text-xs m-2 max-sm:w-[100vw] p-2 flex justify-center items-center ">
         <p>Lukn Developments &copy; {currentYear} All Rights Reserved</p>
       </footer>
     );
